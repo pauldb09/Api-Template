ad952d2d7d295c228ed071de075a132524a5adf3
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2022-04-17 19:43:39 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports.Router = class Home extends Router {
     constructor() {
         super();
         this.get('/', async function(req, res) {
-            console.log("Got a request");
+            res.send("Hello word")
         });
     }
 };
